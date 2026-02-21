797470d94f2720275c4bc0e92e18145b7a55489b
Wasimsaher <waseemsaher99@gmail.com>
2026-02-21 13:03:24 +0200

    api setup

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('posts' , [PostController::class , 'index']);
+
+Route::get('/posts/create/' , [PostController::class , 'create']);
+
+Route::post('/posts/store' , [PostController::class , 'store']);
+
+Route::get('/posts/{id}' , [PostController::class , 'show']);
+
+Route::get('/posts/{id}/edit' , [PostController::class , 'edit']);
+
+Route::put('/posts/{id}' , [PostController::class , 'update']);
+
+Route::delete('/posts/{id}' , [PostController::class , 'destroy']);
