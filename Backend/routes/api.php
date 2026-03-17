da530f4653f835089c0e79b83b40b08ca91e4c72
Wasimsaher <wasimsaher@example.com>
2026-03-17 14:16:32 +0200

    remove blade

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,11 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiPostController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::apiResource('posts', ApiPostController::class);
